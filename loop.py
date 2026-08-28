088e441bfc7d6cabbd3ec670eb8821ae80197475
archana <archxna10@gmail.com>
2026-08-28 12:59:20 +0530

    added for loop prog

---
+++ loop.py
@@ -0,0 +1,17 @@
+# Looping Statements in Python
+
+print("Numbers from 1 to 10:")
+
+for i in range(1, 11):
+    print(i)
+
+print("\nEven numbers:")
+
+for i in range(2, 11, 2):
+    print(i)
+
+print("\nMultiplication table of 5:")
+
+for i in range(1, 11):
+    print("5 x", i, "=", 5 * i)
+
